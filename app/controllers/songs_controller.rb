75db291b64802589166a50a9f62292111dc6d89e
George  Pianka <gpianka1@gmail.com>
2019-02-16 15:51:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,7 @@ class SongsController < ApplicationController
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
     else
+      @song = Song.find(params[:id])
       
     end
   end	  end
