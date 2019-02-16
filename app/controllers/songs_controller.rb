80f655f9d6f46eca7e31449184d3ba74e0bea62b
George  Pianka <gpianka1@gmail.com>
2019-02-16 15:45:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])	    if params[:artist_id]
+    if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
